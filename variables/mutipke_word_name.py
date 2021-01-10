e857589862e2135d813d6584900e00991744d159
DESKTOP-VAA8BBC\Dell-i7 <pattaraporn.chaw@ku.th>
2021-01-10 11:09:39 +0700

    Add new 3 file in folder variables

---
+++ variables/mutipke_word_name.py
@@ -0,0 +1,11 @@
+# Camel Case
+myVariableName = "Pattaraporn Chawakijsophon"
+# ส่วนมากใช้ในJava
+
+# Pascal Case
+MyVariableName = "Pattaraporn Chawakijsophon"
+# Java
+
+# Snake Case
+my_variable_name = "Pattaraporn Chawakijsophon"
+# Python
---
+++ variables/name.py
@@ -0,0 +1,13 @@
+# สามารถประกาศได้
+myvar = "Ying"
+my_var = "Ying"
+_my_var_ = "Ying"
+myvar = "Ying"
+MYVAR = "Ying"
+# ตัวแปรตัวใหญ่ทั้งหมดคือตัวแปรที่ไม่อนุญาติให้ไปเปลี่ยนค่ามัน!!!!
+myvar2 = "Ying"
+
+# ไม่สามารถประกาศได้
+# 2myvar = "Ying"
+# my-var = "Ying"
+# my var = "Ying"
